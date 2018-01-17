5176c2984cffc310abe5eb4959cfdc5c667ea64a
willyspinner <wilsonjusuf1998@gmail.com>
2018-01-16 23:01:12 -0800

    added nodemon, views and public folder, and configured routing in app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //Entry Point for the web app
 let express = require("express");
+let a = 21;
+
 let app = express();
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let methodOverride = require("method-override");
-
+let path = require('path');
 //Allowing JS and CSS to run somoothly and setting up a public directory for the css
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('views', 'views');
+app.set('view engine','ejs');
 //Connecting to database
 mongoose.createConnection("mongodb://localhost/ScoreKeeper");
 
@@ -18,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.get('/', function(req,res){
-	res.sendFile("./index.html");
-})
+	//res.sendFile("./index.html");
+          res.render('index',{});
+});
 
 //I haven't migrated to ejs yet.
 
 app.listen(8000, function(){
-	console.log("Server is running");
+	console.log("Server is running on 8000");
 });
-
