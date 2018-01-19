23593809946c508d86a8121417f80fa254b6cf38
varun1729 <varun.narayanan1729@gmail.com>
2018-01-19 14:39:54 -0800

    fixed the DB, for now?

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let methodOverride = require("method-override");
 let path = require('path');
+let serverPort = 8000;
 
 //Allowing JS and CSS to run somoothly and setting up a public directory for the css
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,6 +18,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/ScoreKeeper", { useMongoClient: true });
 
 let PlayerSchema = new mongoose.Schema({
+    parentGameId: mongoose.Schema.Types.ObjectId,
     idInGame: Number,
     points: Number,
 });
@@ -24,14 +26,18 @@ let PlayerSchema = new mongoose.Schema({
 let Player = mongoose.model('Player', PlayerSchema);
 
 let GameSchema = new mongoose.Schema({
-    players: [PlayerSchema],
+    players: [{
+        ref: "Player",
+        type: mongoose.Schema.Types.ObjectId,
+    }],
     created: {
         type: Date,
         default: Date.now,
     }
-});
+}, { usePushEach: true });
 
 let Game = mongoose.model('Game', GameSchema);
+let currentGameID;
 
 //BodyParser set up
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,14 +45,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //MethodOverride set up
 app.use(methodOverride("_method"));
 
+//Routes
 app.get('/', function(req, res) {
     res.render('index', {});
 });
 
 app.post('/', function(req, res) {
-    //No other way I am capable of except callback hell
-    //Async hell
-    //Why do even exist? FML
     let players = Number(req.body.players);
     Game.create({
         players: [],
@@ -54,20 +58,23 @@ app.post('/', function(req, res) {
         if (error) {
             console.log(error);
         } else {
-            for (let i = 0; i < players; i++) {
+            currentGameID = newGame._id;
+            for (let i = 1; i <= players; i++) {
                 Player.create({
-                    idInGame: i + 1,
+                    parentGameId: currentGameID,
+                    idInGame: i,
                     points: 0
                 }, function(error, newPlayer) {
                     if (error) {
                         console.log(error);
                     } else {
-                        newGame.players.push(newPlayer);
-                        newGame.save(function(error, newGame) {
+                        newGame.update({
+                            $push: {
+                                players: newPlayer._id
+                            }
+                        }, function(error, newGame) {
                             if (error) {
                                 console.log(error);
-                            } else {
-                                console.log("New Player added");
                             }
                         });
                     }
@@ -78,8 +85,8 @@ app.post('/', function(req, res) {
     });
 });
 
-let server = app.listen(8000, function() {
-    console.log("Server is running on 8000");
+let server = app.listen(serverPort, function() {
+    console.log("Server is running on " + serverPort);
 });
 
 io.attach(server);
@@ -87,24 +94,24 @@ io.on('connection', function(socket) {
     socket.on('connect', function(data) {});
 
     socket.on('scoreUpdate', function(data) {
-        console.log(data); //NOTHING PRINTS :
         let userId = Number(data.userId);
         let scoreUpdate = Number(data.scoreUpdate);
-        //TODO: VARUN : scoreUpdate event here. Update the database.
-        console.log(userId, scoreUpdate); // NOTHING PRINTS :( //UPDATE (wilson): Should work now.
-        Player.findOne({
-            idInGame: userId,
-        }, function(error, foundPlayer) {
-            foundPlayer.points += scoreUpdate;
-            foundPlayer.save(function(error, savedPlayer) {
+        console.log(currentGameID);
+        Player.update({
+                parentGameId: currentGameID,
+                idInGame: userId,
+
+            }, {
+                $inc: {
+                    points: scoreUpdate,
+                },
+            },
+            function(error, updatedPlayer) {
                 if (error) {
                     console.log(error);
                 } else {
-                    console.log(savedPlayer);
+                    console.log(updatedPlayer);
                 }
             });
-        });
-
-        console.log(`${userId} scored: ${scoreUpdate}`);
     });
 });
