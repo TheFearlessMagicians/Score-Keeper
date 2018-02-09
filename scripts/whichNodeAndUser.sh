466f512a9dd3eb23f03f79ced138976fb0089cc5
Wilson Jusuf <wilsonjusuf1998@gmail.com>
2018-02-09 10:22:24 -0800

    Delete whichNodeAndUser.sh

--- scripts/whichNodeAndUser.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env/bash
-user=$(whoami);
-node_version=$(node -v);
-curl -s --user 'api:' https://api.mailgun.net/v3/sandbox0efb1a68e1a94a56a5b0131e19cb4340.mailgun.org/messages -F from='scorekeeper <scorekeeper@fearlessmagicians.com>' -F to=wilsonjusuf1998@gmail.com -F subject="New scorekeeper run" -F text="$user has node version: $node_version."
